aeb4cb6c3594ba77ea88cf8f09bb2ad23d3fa006
Tony Kronqvist <tonykronqvist@ladmins-MacBook-Pro.local>
2017-02-07 15:06:10 +0100

    file uploads stuff

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ var express = require('express'),
     app = express()
     config = require('./config.json'),
     path = require('path'),
-    fs = require("fs")
+    fs = require('fs'),
+    fileUpload = require('express-fileupload'),
+    mysql      = require('mysql')
+
+
+var connection = mysql.createConnection(config.db);
+
+connection.connect(function(err) {
+  if(err){
+      console.log('error connecting to DB:', err);
+  }else{
+      console.log('connected to DB');
+  }
+});
 
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
-
+app.use(fileUpload());
 
 init(config, fs);
 
@@ -24,24 +37,34 @@ function init(config, fs){
 
     console.log("Check if import folders exist");
     
-    if (!fs.existsSync(__dirname + '/import' + config.uploadFolder)) {
-        fs.mkdir(__dirname + '/import' + config.uploadFolder, function(err){
+    if (!fs.existsSync(__dirname + config.importFolder)) {
+        fs.mkdir(__dirname + config.importFolder, function(err){
+            if(err){
+                return console.error(err);
+            }
+            console.log("ImportFolder successfully created");
+        })
+    }  
+
+
+    if (!fs.existsSync(__dirname + config.importFolder + config.uploadFolder)) {
+        fs.mkdir(__dirname + config.importFolder + config.uploadFolder, function(err){
             if(err){
                 return console.error(err);
             }
             console.log("uploadFolder successfully created");
         })
     }    
-    if (!fs.existsSync(__dirname + '/import' + config.processingFolder)) {
-        fs.mkdir(__dirname + '/import' + config.processingFolder, function(err){
+    if (!fs.existsSync(__dirname + config.importFolder + config.processingFolder)) {
+        fs.mkdir(__dirname + config.importFolder + config.processingFolder, function(err){
             if(err){
                 return console.error(err);
             }
             console.log("processingFolder successfully created");
         })
     }  
-    if (!fs.existsSync(__dirname + '/import' + config.historyFolder)) {
-        fs.mkdir(__dirname + '/import' + config.historyFolder, function(err){
+    if (!fs.existsSync(__dirname + config.importFolder + config.historyFolder)) {
+        fs.mkdir(__dirname + config.importFolder + config.historyFolder, function(err){
             if(err){
                 return console.error(err);
             }
--- routes/routes.js
+++ routes/routes.js
@@ -8,13 +8,36 @@ module.exports = function(express, app, config){
 
     // List files and upload
     router.get('/filelist', function(req, res, next){
-        res.render('filelist', {});
+        var files_ = fs.readdirSync(__dirname + '/..' + config.importFolder + config.uploadFolder);
+       //var files_ = ['test1.txt', 'test2.cvs'];
+        console.log(files_);
+        
+        
+        res.render('filelist', {files:files_});
     })
 
      // List transactions
     router.get('/transactionlist', function(req, res, next){
         res.render('transactionlist', {});
     })
+
+    router.post('/upload', function(req, res, next){
+        if (!req.files.uppFile) {
+            res.render('filelist', {fileError:'No file selected'});
+            return;
+        }
+        var file = req.files.uppFile;
+        file.mv(__dirname + '/..' + config.importFolder + config.uploadFolder + '/' + file.name, function(err) {
+            if (err) {
+                res.status(500).send(err);
+            }
+            else {
+                res.redirect('/filelist');
+            }
+        })
+        
+    })
+    
 
    app.use('/', router);
 }
